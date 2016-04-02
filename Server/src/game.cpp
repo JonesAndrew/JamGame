2a968c825c2494c59a128b8467bda38597e8b8b7
knotfold <andrewjones@Andrews-MacBook-Pro-2.local>
2016-04-02 16:51:57 -0400

    walls

--- Server/src/game.cpp
+++ Server/src/game.cpp
@@ -13,10 +13,10 @@ void Game::start() {
     tick=0;
     actorCount=0;
 
-    makeActor(std::make_shared<Wall>(VECTOR2(256,27),VECTOR2(512,20)));
-    makeActor(std::make_shared<Wall>(VECTOR2(256,517),VECTOR2(512,20)));
-    makeActor(std::make_shared<Wall>(VECTOR2(27,256),VECTOR2(20,512)));
-    makeActor(std::make_shared<Wall>(VECTOR2(517,256),VECTOR2(20,512)));
+    makeActor(std::make_shared<Wall>(VECTOR2(256,12),VECTOR2(512,20)));
+    makeActor(std::make_shared<Wall>(VECTOR2(256,490),VECTOR2(512,20)));
+    makeActor(std::make_shared<Wall>(VECTOR2(22,256),VECTOR2(20,512)));
+    makeActor(std::make_shared<Wall>(VECTOR2(490,256),VECTOR2(20,512)));
 
     for (int i=0; i<2; i++) {
         gamePlayers.push_back(std::make_shared<Player>());
